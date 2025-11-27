3743f0c7ff59f86b70a2245a2be8749651338342
SkIym <abramjosh13@gmail.com>
2025-11-27 17:34:50 +0800

    complete column mapping

--- mappers/geog_archive_mapper.py
+++ mappers/geog_archive_mapper.py
@@ -8,8 +8,9 @@ COLUMN_MAPPING = {
     "Longitude": "longitude",
     "Main Area/s Affected / Location": "hasLocation",  
     "Additional Perils/Disaster Sub-Type Occurences (Compound Disaster, e.g. Typhoon Haiyan = rain + wind + storm surge)": "hasSubtype",
-    "PREPAREDNESS_No.ofEvacuationCenters": "evacuationCenters",
-    "IMPACT_NumberofAffectedAreas_Barangays": "affectedBarangays",
+    "PREPAREDNESS_Announcements_Warnings Released / Status Alert or Alert/ State of Calamity": "State of Calamity",
+    "PREPAREDNESS_No. of Evacuation Centers": "evacuationCenters",
+    "IMPACT_Number of Affected Areas_Barangays": "affectedBarangays",
     "IMPACT_Casualties_Dead_Total": "dead",
     "IMPACT_Casualties_Injured_Total": "injured",
     "IMPACT_Casualties_Missing_Total": "missing",
@@ -17,15 +18,31 @@ COLUMN_MAPPING = {
     "IMPACT_Affected_Persons": "affectedPersons",
     "IMPACT_Evacuated_Families": "displacedFamilies",
     "IMPACT_Evacuated_Persons": "displacedPersons",
-    "IMPACT_DamagetoProperties_Houses_Fully": "totallyDamagedHouses",
-    "IMPACT_DamagetoProperties_Houses_Partially": "partiallyDamagedHouses",
-    "IMPACT_DamagetoProperties_Infrastructure(inMillions)": "infraDamageAmount",
-    "IMPACT_DamagetoProperties_Agriculture(inMillions)": "agricultureDamageAmount",
-    "IMPACT_DamagetoProperties_Private/Commercial(inMillions)": "commercialDamageAmount",
-    "IMPACT_StatusofLifelines_ElectricityorPowerSupply": "isPowerDisrupted",
-    "IMPACT_StatusofLifelines_CommunicationLines": "isCommunicationDisrupted",
-    
-
+    "IMPACT_Damages to Properties_Houses_Fully": "totallyDamagedHouses",
+    "IMPACT_Damages to Properties_Houses_Partially": "partiallyDamagedHouses",
+    "IMPACT_Damages to Properties_Infrastructure (in Millions)": "infraDamageAmount",
+    "IMPACT_Damages to Properties_Agriculture (in Millions)": "agricultureDamageAmount",
+    "IMPACT_Damages to Properties_Private/Commercial (in Millions)": "commercialDamageAmount",
+    "IMPACT_Status of Lifelines_Electricity or Power Supply": "PowerAffected",
+    "IMPACT_Status of Lifelines_Communication Lines": "CommunicationAffected",
+    "IMPACT_Status of Lifelines_Transportation_Roads and Bridges": "RoadAndBridgesAffected",
+    "IMPACT_Status of Lifelines_Transportation_Seaports": "SeaportsAffected",
+    "IMPACT_Status of Lifelines_Transportation_Airports": "AirportsAffected",
+    "IMPACT_Status of Lifelines_Water_Dams and other Reservoirs": "areDamsAffected",
+    "IMPACT_Status of Lifelines_Water_Tap": "isTapAffected",
+    "Allocated Funds for the Affected Area/s": "allocatedFunds",
+    "NGO-LGU Support Units Present": "agencyLGUsPresent",
+    "International Organizations Present": "internationalOrgsPresent",
+    "Amount of Donation from International Organizations (including local NGOs)": "amoungNGOs",
+    "Supply of Relief Goods_Canned Goods, Rice, etc._Cost": "itemCost",    # itemTypeOrNeeds: Canned Goods, Rice
+    "Supply of Relief Goods_Canned Goods, Rice, etc._Quantity": "itemQty", # itemTypeOrNeeds: Canned Goods, Rice
+    "Supply of Relief Goods_Water_Cost": "itemCost",    # itemTypeOrNeeds: Water
+    "Supply of Relief Goods_Water_Quantity": "itemQty", # itemTypeOrNeeds: Water
+    "Supply of Relief Goods_Clothing_Cost": "itemCost",    # itemTypeOrNeeds: Clothing
+    "Supply of Relief Goods_Clothing_Quantity": "itemQty", # itemTypeOrNeeds: Clothing
+    "Supply of Relief Goods_Medicine_Cost": "itemCost",    # itemTypeOrNeeds: Medicine
+    "Supply of Relief Goods_Medicine_Quantity": "itemQty", # itemTypeOrNeeds: Medicine
+    "Supply of Relief Goods_Items Not Specified (Cost)": "itemCost", # itemTypeTypeOrNeeds: Others
 
 }
 
