b856ac038a51239761454adc5ebdb36556019af9
SkIym <abramjosh13@gmail.com>
2026-01-21 19:26:12 +0800

    feat(mappings): add udf for location splitter

--- mappers/udfs/gda_udfs.py
+++ mappers/udfs/gda_udfs.py
@@ -2,7 +2,7 @@ base = "https://sakuna.ph/"
 
 @udf(
     fun_id="https://sakuna.ph/toTypeIRI",
-    hasType="http://users.ugent.be/~bjdmeest/function/grel.ttl#valueParam"
+    hasType="http://users.ugent.be/~bjdmeest/function/grel.ttl#valueParameter"
 )
 def to_type_iri(hasType: str):
     fixedIRI = (
@@ -18,7 +18,7 @@ def to_type_iri(hasType: str):
 
 @udf(
     fun_id="https://sakuna.ph/cleanName",
-    eventName="http://users.ugent.be/~bjdmeest/function/grel.ttl#valueParam"
+    eventName="http://users.ugent.be/~bjdmeest/function/grel.ttl#valueParameter"
 )
 def clean_event_name(eventName: str):
     fixedStr = (
@@ -26,3 +26,18 @@ def clean_event_name(eventName: str):
 
     )
     return fixedStr
+
+
+@udf(
+    fun_id="https://sakuna.ph/matchLocationsToIRI",
+    locations="http://users.ugent.be/~bjdmeest/function/grel.ttl#valueParameter"
+)
+def match_locs_to_IRI(locations: str):
+    locs =  [l.strip() for l in locations.split("|")]
+
+    for loc in locs:
+        
+
+
+    return locs
+
