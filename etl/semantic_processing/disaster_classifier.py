e78c2d2027d9a20f061f834da58589e36c565d31
SkIym <abramjosh13@gmail.com>
2026-03-16 12:09:56 +0800

    BREAKING CHANGE: refactor disaster type classes into skos concepts

--- etl/semantic_processing/disaster_classifier.py
+++ etl/semantic_processing/disaster_classifier.py
@@ -1,65 +1,76 @@
 from sentence_transformers import SentenceTransformer, util
 from typing import List, Tuple
 from rdflib import Graph, RDF, Namespace, URIRef, RDFS, SKOS
+from rdflib.namespace import DefinedNamespace
+
+SKG = Namespace("https://sakuna.ph/")
+
+LEAF_CLASS_QUERY = """
+SELECT DISTINCT ?c ?defi WHERE {
+    ?c rdfs:subClassOf+ skg:DisasterType ;
+       skos:definition ?defi .
+    FILTER NOT EXISTS {
+        ?child rdfs:subClassOf ?c .
+        ?child rdfs:subClassOf+ skg:DisasterType .
+    }
+}
+"""
+
+WAVE_ACTION_QUERY = """
+SELECT ?defi WHERE {
+    skg:WaveAction skos:definition ?defi .
+}
+LIMIT 1
+"""
+
+ONTOLOGY_PATH = "../ontology/sakunagraph.ttl"
+INIT_NS: dict[str, Namespace | type[DefinedNamespace]] = {"rdfs": RDFS, "skg": SKG, "skos": SKOS}
+
+
+def _uri_to_label(graph: Graph, uri: URIRef) -> str:
+    return graph.namespace_manager.normalizeUri(uri).split(":", 1)[1]
+
+
+def _load_classes(graph: Graph) -> dict[str, str]:
+    """Load leaf DisasterType classes + WaveAction, keyed by definition."""
+    classes: dict[str, str] = {}
+
+    for row in graph.query(LEAF_CLASS_QUERY, initNs=INIT_NS):
+        label = _uri_to_label(graph, row.c)
+        classes[row.defi.value] = label
+
+    for row in graph.query(WAVE_ACTION_QUERY, initNs=INIT_NS):
+        classes[row.defi.value] = "WaveAction"
+
+    return classes
+
 
 class DisasterClassifier:
-    def __init__(self, model_name: str):
-        # Load model 
+    def __init__(self, model_name: str, ontology_path: str = ONTOLOGY_PATH):
         self.model = SentenceTransformer(model_name)
 
-        # Load classes
-        # with open(classes_path, "r", encoding="utf-8") as f:
-        #     self.classes: dict[str, str] = json.load(f)
-
-        # Load classes
-        SKG = Namespace("https://sakuna.ph/")
         g = Graph()
-        g.parse("../ontology/sakunagraph.ttl")
+        g.parse(ontology_path)
 
-        self.classes: dict[str, str] = {}
-
-        q = """
-        SELECT DISTINCT ?c ?defi WHERE {
-        ?c rdfs:subClassOf+ skg:DisasterType ;
-            skos:definition ?defi ;
-        FILTER NOT EXISTS {
-            ?child rdfs:subClassOf ?c .
-        }
-        }
-        """
-        for row in g.query(q, initNs={"rdfs": RDFS, "skg": SKG, "skos": SKOS}):
-            # print(g.namespace_manager.normalizeUri(row.c).split(":", 1)[1], row.defi)
-
-            clsf = g.namespace_manager.normalizeUri(row.c).split(":", 1)[1]
-            self.classes[row.defi.value] = clsf
-
-        # Precompute embeddings for class names
+        self.classes = _load_classes(g)
         self.class_keys = list(self.classes.keys())
         self.class_labels = list(self.classes.values())
         self.embeddings = self.model.encode(self.class_keys, convert_to_tensor=True)
 
     def classify(self, texts: List[str]) -> List[Tuple[str, float]]:
         """
-        Classify a list of incident types.
+        Classify a list of incident descriptions.
         Returns a list of (predicted_class_label, similarity_score) tuples.
         """
-        # Encode all texts at once
         text_embeddings = self.model.encode(texts, convert_to_tensor=True)
+        scores = util.cos_sim(text_embeddings, self.embeddings)
 
-        # Compute cosine similarities in one shot
-        # scores = util.dot_score(text_embeddings, self.embeddings)
-        scores = util.cos_sim(text_embeddings, self.embeddings)  
+        return [
+            (self.class_labels[row.argmax().item()], row.max().item())
+            for row in scores
+        ]
 
-        # Find best class per text
-        results = []
-        for i, score_vector in enumerate(scores):
-            best_idx = score_vector.argmax().item()
-            results.append((self.class_labels[best_idx], score_vector[best_idx].item()))
-        return results
 
 DISASTER_CLASSIFIER = DisasterClassifier(
-    # model_name="all-MiniLM-L6-v2",
     model_name="all-mpnet-base-v2",
-    # model_name="multi-qa-mpnet-base-dot-v1",
 )
-
